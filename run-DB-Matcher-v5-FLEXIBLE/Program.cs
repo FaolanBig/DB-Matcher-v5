a72321bf2ae7876227fc45b55abed1539839e3f7
FaolanBig <Faolan.Big@web.de>
2024-06-28 14:38:13 +0200

    update

--- run-DB-Matcher-v5-FLEXIBLE/Program.cs
+++ run-DB-Matcher-v5-FLEXIBLE/Program.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿/*
+    !!!This program will install DB-Matcher on your system by building it from source files!!!
+
+    The DB-Matcher is an easy-to-use console application written in C# and based on the .NET framework. 
+    The DB-Matcher can merge two databases in Excel format (*.xlsx, *.xls). 
+    It follows the following algorithms in order of importance: Levenshtein distance, Hamming distance, Jaccard index. 
+    The DB-Matcher takes you by the hand at all times and guides you through the data matching process    
+
+    Copyright (C) 2024  Carl Öttinger (Carl Oettinger)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    You can contact me in the following ways:
+        EMail: oettinger.carl@web.de, big-programming@web.de
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
