0c9804c9faff37a8688b124ac34164ba703133c3
FaolanBig <93519016+FaolanBig@users.noreply.github.com>
2025-01-03 23:38:00 +0100

    newReleaseFor_DB-Matcher-v5.sh fixing capitalization

--- newReleaseFor_DB-Matcher-v5.sh
+++ newReleaseFor_DB-Matcher-v5.sh
@@ -17,7 +17,7 @@ echo "cleaning..."
 rm -rf $REPO_NAME
 echo "cleaning successful"
 
-echo "Cloning repository..."
+echo "cloning repository..."
 git clone "$REPO_URL" "$REPO_NAME" || { echo "Error: git clone failed"; exit 1; }
 cd "$REPO_NAME" || { echo "Error: repository not found"; exit 1; }
 echo "cloning successful"
@@ -33,7 +33,7 @@ echo "creatung successful"
 # Function to generate SHA256 hash files
 generate_hashes() {
   local dir=$1
-  echo "Generating SHA256 hashes in $dir..."
+  echo "generating SHA256 hashes in $dir..."
   for file in "$dir"/*; do
     if [ -f "$file" ]; then
       echo "hashing file $file"
@@ -44,7 +44,7 @@ generate_hashes() {
 }
 
 # Build for Linux
-echo "Building for Linux..."
+echo "building for Linux..."
 dotnet publish "$PROJECT_FILE" -c Release -r linux-x64 --self-contained true --nologo -v q --property WarningLevel=0 /clp:ErrorsOnly /p:PublishSingleFile=true -o "../$BUILD_DIR/linux" || { echo "Error: Linux build failed"; exit 1; }
 cd "../$BUILD_DIR/linux" || exit
 echo "compressing for linux as .zip"
@@ -56,7 +56,7 @@ echo "success"
 cd ../../"$REPO_NAME" || exit
 
 # Build for Windows
-echo "Building for Windows..."
+echo "building for Windows..."
 dotnet publish "$PROJECT_FILE" -c Release -r win-x64 --self-contained true --nologo -v q --property WarningLevel=0 /clp:ErrorsOnly /p:PublishSingleFile=true -o "../$BUILD_DIR/windows" || { echo "Error: Windows build failed"; exit 1; }
 cd "../$BUILD_DIR/windows" || exit
 echo "compressing for windows as .zip"
@@ -73,7 +73,7 @@ generate_hashes "../$BUILD_DIR/"
 echo "hashing finished successfully"
 
 # Create GitHub Release
-echo "Creating GitHub release..."
+echo "creating GitHub release..."
 LATEST_TAG=$(echo "auto_gen_$(date '+%Y-%m-%d_%H-%M-%S')")
 gh release create "$LATEST_TAG" \
   "../$BUILD_DIR/${LINUX_FILENAME}.zip" \
@@ -94,5 +94,5 @@ rm -rf "../$BUILD_DIR"
 rm -rf "../$REPO_NAME"
 echo "cleaning finished successfully"
 echo " "
-echo "Done, see you soon"
+echo "done, see you soon"
 
